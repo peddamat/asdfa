67100904ae978e5f11a88ad35e059eb4b4ee0ac6
Sumanth Peddamatham <peddamat@gmail.com>
2013-07-10 06:48:05 +0000

    Don't know how this didn't get committed earlier.

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ export QUICK2WIRE_API_HOME=/home/pi/nrf/quick2wire-python-api-master
 export PYTHONPATH=$PYTHONPAT:$QUICK2WIRE_API_HOME
 sudo gpio-admin unexport 8 
 sudo gpio-admin unexport 25
-python3 nRF24L01p.py
+#python3 nRF24L01p.py
+python3 message_proxy.py
 
